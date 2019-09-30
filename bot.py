155097e1a3a4b2ecf711881b12d723c3082a4ecb
Pascal Böhler <pascal.boehler@hotmail.de>
2019-09-30 17:41:39 +0200

    Silenced warnings and added german words to project dict

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import telepot
-from telepot.delegate import per_chat_id, create_open, pave_event_space, include_callback_query_chat_id
 import json
 import Strike
 import time
 
+
 def telegramBot():
-    datasets = []
     datasetsOld = []
-    id = 1 # counter var
-    updateIdOld = 0 # no updates fetched
+    id = 1  # counter var
+    updateIdOld = 0  # no updates fetched
 
     print("Listening....")
 
@@ -17,35 +16,31 @@ def telegramBot():
         data = json.load(open('config.json'))
 
         telegram_bot_token = data['bot']['private_key']
-        updateIdOld = data['bot']['updateId'] # get the old update Id if it exists
+        updateIdOld = data['bot']['updateId']  # get the old update Id if it exists
         bot = telepot.Bot(telegram_bot_token)
 
         if updateIdOld == 0:
             updates = bot.getUpdates()
             if len(updates) != 0:
-                updateIdOld = updates[len(updates)-1]['update_id']
+                updateIdOld = updates[len(updates) - 1]['update_id']
             else:
-                #print("There are no updates available")
                 continue
         else:
             updates = bot.getUpdates(updateIdOld + 1)
             if len(updates) != 0:
                 updateIdOld = updates[len(updates) - 1]['update_id']
             else:
-                #print("There are no updates available")
                 continue
 
         with open('config.json', mode='w') as json_data_file:  # add the new id to file
-            #data = json.load(json_data_file)
             data['bot']['updateId'] = updateIdOld
             try:
-                json_data_file.write(json.dumps(data)) # write data to file, if it fails, catch error
+                json_data_file.write(json.dumps(data))  # write data to file, if it fails, catch error
             except:
-                print(str(time.time()) + ": Error while writing file, please check your permsissions! (need to be "
-                                         "read write (chmod 777 config.json)") # time in milliseconds until the 1.1.1970
+                print(str(time.time()) + ": Error while writing file, please check your permissions! (need to be "
+                                         "read write (chmod 777 config.json)")  # time until the 1.1.1970 in ms
             json_data_file.close()
 
-        #print(updates)
         messages = dict()
 
         groupIds = [update['message']['chat']['id'] for update in updates if 'text' in update['message']]
@@ -60,19 +55,10 @@ def telegramBot():
         for messageSet in messages.values():
 
             chatId = groupIds[i]
-            title = ""
-            description = ""
-            dateTime = ""
-            startingPoint = ""
-            startingTime = "" # could be a time stamp is a UnicodeAttribute now!
-            endPoint = ""
-            routeLength = ""
-
-            jsonFeed = []
 
             for message in messageSet:
                 # TODO Fix help message to only be displayed once
-                if (message == "help"):
+                if message == "help":
                     bot.sendMessage(text="Jedes “Property in einer Zeile”\nWenn nicht bekannt NONE "
                                          "einfügen\n\nSchema:\nTitel\nBeschreibung\nDatum und Uhrzeit\nStartpunkt / "
                                          "Treffpunkt\nEndpunkt\nRoutenlänge\nWebsite Veranstalter (wenn nicht bekannt "
@@ -83,23 +69,23 @@ def telegramBot():
                     jsonFeed = message.split('\n')
                     if len(jsonFeed) == 10:
                         if jsonFeed[0].lower().strip() == 'bot:':
-                            if jsonFeed[1].lower().strip() == 'none': # Title
+                            if jsonFeed[1].lower().strip() == 'none':  # Title
                                 jsonFeed[1] = ' '
-                            if jsonFeed[2].lower().strip() == 'none': # Description
+                            if jsonFeed[2].lower().strip() == 'none':  # Description
                                 jsonFeed[2] == ' '
-                            if jsonFeed[3].lower().strip() == 'none': # organisation
+                            if jsonFeed[3].lower().strip() == 'none':  # organisation
                                 jsonFeed[3] = ' '
-                            if jsonFeed[4].lower().strip() == 'none': # Date
+                            if jsonFeed[4].lower().strip() == 'none':  # Date
                                 jsonFeed[4] = ' '
-                            if jsonFeed[5].lower().strip() == 'none': # Time
+                            if jsonFeed[5].lower().strip() == 'none':  # Time
                                 jsonFeed[5] = ' '
-                            if jsonFeed[6].lower().strip() == 'none': # meetingPoint / StartPoint
+                            if jsonFeed[6].lower().strip() == 'none':  # meetingPoint / StartPoint
                                 jsonFeed[6] = ' '
-                            if jsonFeed[7].lower().strip() == 'none': # endPoint
+                            if jsonFeed[7].lower().strip() == 'none':  # endPoint
                                 jsonFeed[7] = ' '
-                            if jsonFeed[8].lower().strip() == 'none': # routing
+                            if jsonFeed[8].lower().strip() == 'none':  # routing
                                 jsonFeed[8] = ' '
-                            if jsonFeed[9].lower().strip() == 'none': # url
+                            if jsonFeed[9].lower().strip() == 'none':  # url
                                 jsonFeed[9] = ' '
                             else:
                                 jsonFeed[9] = "https://" + jsonFeed[9]
@@ -107,8 +93,6 @@ def telegramBot():
 
                 except:
                     continue
-
-        #print(datasets)
 
         if datasets == datasetsOld:
             continue
