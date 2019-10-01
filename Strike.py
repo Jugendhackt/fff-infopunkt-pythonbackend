70b9d0447e954de284cb7df89ba479d22c217c8f
Marvin <24806960+magicmarvman@users.noreply.github.com>
2019-10-01 10:58:56 +0200

    Prettify Strike model

--- Strike.py
+++ Strike.py
@@ -1,32 +1,25 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute
 import json
-
 data = json.load(open('config.json'))
 
 class StrikeModel(Model):
     """
     A DynamoDB Strike
     """
-
     class Meta:
         table_name = "FFF-Strikes"
-
-        # AWS Config
-        # Specifies the region
+        # AWS Config Specifies the region
         region = 'eu-central-1'
         # Access keys
         aws_access_key_id = data['aws']['access_key']
         aws_secret_access_key = data['aws']['private_key']
-
         '''
-        # If hosted by us :-)
-        #host = "http://116.203.195.66:4567"
+        # If hosted by us :-) host = "http://116.203.195.66:4567"
         host = data['aws']['host']
         aws_access_key_id = data['aws']['access_key']
         aws_secret_access_key = data['aws']['private_key']
         '''
-
     # The model:
     strikeId = NumberAttribute(hash_key=True)
     date = UnicodeAttribute()
